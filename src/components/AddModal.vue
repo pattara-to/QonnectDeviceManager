3ee820d6400e5133be3e80b068d4738455d3c486
Pattara <pattaraw.to@gmail.com>
2024-09-20 14:09:26 +0700

    change grid

--- src/components/AddModal.vue
+++ src/components/AddModal.vue
@@ -29,13 +29,14 @@ const closeModalOnOutsideClick = (event) => {
 <template>
     <div class="modal block fixed z-1 w-full h-full bg-black/70" @click="closeModalOnOutsideClick">
         <transition name="slide">
-            <div v-if="props.modalIsOpen"
+            <div
+                v-if="props.modalIsOpen"
                 class="modal-content p-8 rounded-xl shadow-lg bg-white fixed right-0 h-[93%] w-[90%] md:w-[30%] flex flex-col items-center overflow-auto transition-all duration-500"
-                @click.stop>
-                
+                @click.stop
+            >
                 <!-- Add Device Title -->
                 <h2 class="text-2xl font-bold text-gray-800 mb-6">Add Device</h2>
-                
+
                 <!-- Device Image -->
                 <img src="../assets/machine.png" alt="machine" class="w-40 h-auto my-[3rem] object-cover" />
 
@@ -44,34 +45,59 @@ const closeModalOnOutsideClick = (event) => {
                     <!-- Machine Name -->
                     <div class="flex flex-col w-full">
                         <label class="font-semibold text-gray-700">Machine Name</label>
-                        <input type="text" v-model="deviceData.name" class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" required/>
+                        <input
+                            type="text"
+                            v-model="deviceData.name"
+                            class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+                            required
+                        />
                     </div>
 
                     <!-- MAC Address -->
                     <div class="flex flex-col w-full">
                         <label class="font-semibold text-gray-700">MAC Address</label>
-                        <input type="text" v-model="deviceData.MAC" class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full" />
+                        <input
+                            type="text"
+                            v-model="deviceData.MAC"
+                            class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+                        />
                     </div>
 
                     <!-- Description -->
                     <div class="flex flex-col w-full">
                         <label class="font-semibold text-gray-700">Description</label>
-                        <textarea v-model="deviceData.description" rows="4" class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"></textarea>
+                        <textarea
+                            v-model="deviceData.description"
+                            rows="4"
+                            class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+                        ></textarea>
                     </div>
 
                     <!-- Location -->
                     <div class="flex flex-col w-full">
                         <label class="font-semibold text-gray-700">Location</label>
-                        <textarea v-model="deviceData.location" rows="4" class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"></textarea>
+                        <textarea
+                            v-model="deviceData.location"
+                            rows="4"
+                            class="bg-gray-100 border border-gray-300 rounded-lg py-2 px-3 mt-1 resize-none focus:outline-none focus:ring-2 focus:ring-blue-500 w-full"
+                        ></textarea>
                     </div>
                 </div>
 
                 <!-- Buttons -->
                 <div class="flex justify-end w-full mt-6 space-x-4">
-                    <button class="px-4 py-2 border border-red-500 text-red-500 rounded-lg hover:bg-red-500 hover:text-white transition"
-                        @click="props.toggleModal">Cancel</button>
-                    <button class="px-4 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-500 hover:text-white transition"
-                        @click="addDevice()">Add</button>
+                    <button
+                        class="px-4 py-2 border border-red-500 text-red-500 rounded-lg hover:bg-red-500 hover:text-white transition"
+                        @click="props.toggleModal"
+                    >
+                        Cancel
+                    </button>
+                    <button
+                        class="px-4 py-2 border border-green-500 text-green-500 rounded-lg hover:bg-green-500 hover:text-white transition"
+                        @click="addDevice()"
+                    >
+                        Add
+                    </button>
                 </div>
             </div>
         </transition>
--- src/views/DevicesView.vue
+++ src/views/DevicesView.vue
@@ -28,22 +28,21 @@ const addDevice = async (deviceData) => {
 </script>
 
 <template>
-  <Navbar />
-  <AddModal :toggleModal="toggleModal" :addDevice="addDevice" :modalIsOpen="modalIsOpen" v-show="modalIsOpen" />
-  <div class="flex flex-col h-[93%]">
-    <div class="flex justify-between items-center my-6 px-16">
-      <h1 class="text-2xl font-semibold text-gray-800">Devices</h1>
-      <button
-        @click="toggleModal"
-        class="rounded-lg px-3 py-1 text-m font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-200"
-      >
-        + Add Device
-      </button>
-    </div>
+    <Navbar />
+    <AddModal :toggleModal="toggleModal" :addDevice="addDevice" :modalIsOpen="modalIsOpen" v-show="modalIsOpen" />
+    <div class="flex flex-col h-[93%]">
+        <div class="flex justify-between items-center my-6 px-16">
+            <h1 class="text-2xl font-semibold text-gray-800">Devices</h1>
+            <button
+                @click="toggleModal"
+                class="rounded-lg px-3 py-1 text-m font-medium text-white bg-blue-500 hover:bg-blue-600 transition duration-200"
+            >
+                + Add Device
+            </button>
+        </div>
 
-    <div class="grid grid-cols-5 gap-7 mx-16 mb-2">
-      <Machine class="h-1/2" :device="device" v-for="device in deviceStore.list" :key="device.id" />
+        <div class="grid grid-cols-6 gap-6 mx-16 mb-2">
+            <Machine class="h-1/2" :device="device" v-for="device in deviceStore.list" :key="device.id" />
+        </div>
     </div>
-  </div>
 </template>
-
